0f78445899f922cf69e2fd79bf05c64f0859fd09
meowmeowcomputers <ryansimonleon@gmail.com>
2017-04-28 11:17:43 -0500

    Implemented diagonal movement

--- monsterdodge.py
+++ monsterdodge.py
@@ -2,6 +2,31 @@ import pygame
 from random import randint
 import time
 
+class Character:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speedX = 0
+        self.speedY = 0
+    def update(self):
+        self.x += self.speedX
+        self.y += self.speedY
+#    def display(self):
+#        screen.blit(heroImg, (self.x, self.y))
+
+class Hero(Character):
+    def limit(self):
+        if self.x >460:
+            self.x = 460
+        elif self.x < 12:
+            self.x = 12
+        elif self.y > 428:
+            self.y =428
+        elif self.y < 12:
+            self.y = 12
+
+
+
 def main():
     width = 512
     height = 480
@@ -15,8 +40,7 @@ def main():
     # Game initialization
     time_started = time.time()
     next_action_time = time_started +2
-    heroX = 256
-    heroY = 0
+    hero = Hero(256, 240)
     monsterX = 300
     monsterY = 300
     stop_game = False
@@ -25,14 +49,9 @@ def main():
     monsterImg = pygame.image.load('images/monster.png')
 
     screen.blit(background, (0,0))
-    screen.blit(heroImg, (heroX, heroY))
+    screen.blit(heroImg, (hero.x, hero.y))
     screen.blit(monsterImg, (monsterX, monsterY))
 
-    # def heroMove(x, y):
-    #     screen.blit(background, (0,0))
-    #     heroX += x
-    #     heroY += y
-    #     screen.blit(heroImg, (heroX, heroY))
     pygame.key.set_repeat(10)
 
     while not stop_game:
@@ -42,20 +61,24 @@ def main():
                 stop_game = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    heroX += 10
+                    hero.speedX = 10
                 if event.key == pygame.K_LEFT:
-                    heroX -= 10
+                    hero.speedX = -10
                 if event.key == pygame.K_UP:
-                    heroY -= 10
+                    hero.speedY = -10
                 if event.key == pygame.K_DOWN:
-                    heroY += 10
+                    hero.speedY = 10
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    heroY = heroY
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    heroX = heroX
+                if event.key == pygame.K_DOWN:
+                    hero.speedY = 0
+                elif event.key == pygame.K_UP:
+                    hero.speedY = 0
+                elif event.key == pygame.K_LEFT:
+                    hero.speedX = 0
+                elif event.key == pygame.K_RIGHT:
+                    hero.speedX = 0
         screen.blit(background, (0,0))
-        screen.blit(heroImg, (heroX, heroY))
+        screen.blit(heroImg, (hero.x, hero.y))
         #
         monsterY += randint(1,100)
         monsterX += randint(-100,100)
@@ -68,22 +91,25 @@ def main():
         if monsterX <= 0:
             monsterX += 480
         screen.blit(monsterImg, (monsterX, monsterY))
-        if heroX >460:
-            heroX = 460
-        elif heroX < 12:
-            heroX = 12
-        elif heroY > 428:
-            heroY =428
-        elif heroY < 12:
-            heroY = 12
-        heroHitX = range(heroX, heroX+32)
-        heroHitY = range(heroY, heroY+32)
-        # if monsterX >= heroX and monsterX <= heroX-32 and monsterY >= heroY and monsterY <= heroY +32 and monsterY >= heroY:
+        # if heroX >460:
+        #     heroX = 460
+        # elif heroX < 12:
+        #     heroX = 12
+        # elif heroY > 428:
+        #     heroY =428
+        # elif heroY < 12:
+        #     heroY = 12
+
+        # heroHitX = range(heroX, heroX+32)
+        # heroHitY = range(heroY, heroY+32)
+
+        # # if monsterX >= heroX and monsterX <= heroX-32 and monsterY >= heroY and monsterY <= heroY +32 and monsterY >= heroY:
         #     print('HIT')
 
 
         # Game logic
-
+        hero.update()
+        hero.limit()
         if next_action_time <= int(time.time()):
         #    monster.mov()
             time_started = time.time()
