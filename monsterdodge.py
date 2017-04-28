2dd07fc2bd8eeac506e3848fd230d095d8541fe9
meowmeowcomputers <ryansimonleon@gmail.com>
2017-04-28 14:40:28 -0500

    Changed genre, completed hit detection, started fire command

--- monsterdodge.py
+++ monsterdodge.py
@@ -3,11 +3,12 @@ from random import randint
 import time
 
 class Character:
-    def __init__(self, x, y):
+    def __init__(self, x, y, dire):
         self.x = x
         self.y = y
         self.speedX = 0
         self.speedY = 0
+        self.dire = dire
     def update(self):
         self.x += self.speedX
         self.y += self.speedY
@@ -24,7 +25,42 @@ class Hero(Character):
             self.y =428
         elif self.y < 12:
             self.y = 12
+    def hitDetect(self, targetX, targetY):
+        if self.x + 32 > targetX and targetX + 32 > self.x and self.y + 32 > targetY and targetY + 32 > self.y:
+            return True
 
+class Monster(Character):
+    def limit(self):
+        if self.x >= 512:
+            self.x -= 512
+        if self.y >= 480:
+            self.y -= 480
+        if self.y <= 0:
+            self.y += 480
+        if self.x <= 0:
+            self.x += 480
+    def selfMoveGen(self):
+        mov = randint(1,6)
+        self.dire = mov
+    def selfMove(self):
+        if self.dire == 1: #right
+            self.speedX = 5
+        if self.dire == 2: #left
+            self.speedX = -5
+        if self.dire == 3: #up
+            self.speedY = -5
+        if self.dire == 4: # down
+            self.speedY = 5
+        if self.speedX == 0 or self.speedY == 0:
+            pass
+        else:
+            if self.dire == 6: #stopY
+                self.speedY = 0
+        if self.speedY == 0:
+            pass
+        else:
+            if self.dire == 5: #stopX
+                self.speedX = 0
 
 
 def main():
@@ -40,19 +76,23 @@ def main():
     # Game initialization
     time_started = time.time()
     next_action_time = time_started +2
-    hero = Hero(256, 240)
-    monsterX = 300
-    monsterY = 300
+    hero = Hero(256, 240, 0)
+    monster = Monster(300, 300, 1)
+
     stop_game = False
-    background = pygame.image.load('images/background.png')
-    heroImg = pygame.image.load('link.gif')
+    background = pygame.image.load('space.png')
+    backgroundAlt = pygame.image.load('spaceinverse.png')
+    heroImg = pygame.image.load('crosshair2.png')
     monsterImg = pygame.image.load('images/monster.png')
 
     screen.blit(background, (0,0))
     screen.blit(heroImg, (hero.x, hero.y))
-    screen.blit(monsterImg, (monsterX, monsterY))
+    screen.blit(monsterImg, (monster.x, monster.y))
 
     pygame.key.set_repeat(10)
+
+    hit = 0 #hit detection
+    loopIteration = 0 #loop count
 
     while not stop_game:
             # Event handling
@@ -68,6 +108,11 @@ def main():
                     hero.speedY = -10
                 if event.key == pygame.K_DOWN:
                     hero.speedY = 10
+                if event.key == pygame.K_SPACE:
+                    print('SPACE')
+                    screen.blit(backgroundAlt, (0,0))
+                    pygame.display.update()
+                    pygame.time.delay(5)
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_DOWN:
                     hero.speedY = 0
@@ -78,40 +123,25 @@ def main():
                 elif event.key == pygame.K_RIGHT:
                     hero.speedX = 0
         screen.blit(background, (0,0))
-        screen.blit(heroImg, (hero.x, hero.y))
+        if hero.hitDetect(monster.x, monster.y):
+        #if monster.x + 32 > hero.x and hero.x + 32 > monster.x and monster.y + 32 > hero.y and hero.y + 32 > monster.y:
+            hit +=1
+            print(hit)
         #
-        monsterY += randint(1,100)
-        monsterX += randint(-100,100)
-        if monsterX >= 512:
-            monsterX -= 512
-        if monsterY >= 480:
-            monsterY -= 480
-        if monsterY <= 0:
-            monsterY += 480
-        if monsterX <= 0:
-            monsterX += 480
-        screen.blit(monsterImg, (monsterX, monsterY))
-        # if heroX >460:
-        #     heroX = 460
-        # elif heroX < 12:
-        #     heroX = 12
-        # elif heroY > 428:
-        #     heroY =428
-        # elif heroY < 12:
-        #     heroY = 12
-
-        # heroHitX = range(heroX, heroX+32)
-        # heroHitY = range(heroY, heroY+32)
-
-        # # if monsterX >= heroX and monsterX <= heroX-32 and monsterY >= heroY and monsterY <= heroY +32 and monsterY >= heroY:
-        #     print('HIT')
-
-
         # Game logic
-        hero.update()
+        monster.selfMove()
+        monster.limit()
+        monster.update()
         hero.limit()
+        hero.update()
+        screen.blit(heroImg, (hero.x, hero.y))
+        screen.blit(monsterImg, (monster.x, monster.y))
+
         if next_action_time <= int(time.time()):
-        #    monster.mov()
+            loopIteration += 1
+            print('Loop',loopIteration)
+            print(monster.dire)
+            monster.selfMoveGen()
             time_started = time.time()
             next_action_time = int(time_started) + 2
         # Game display
